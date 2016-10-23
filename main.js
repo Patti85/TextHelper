6f47e5f917caf59598246faae5a5e7cdb91e981c
Patricia Beier <pbeier24@googlemail.com>
2016-10-23 15:20:32 +0200

    fields for keywords

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ var TextHelperApp = angular.module('TextHelperApp', [ ]);
 TextHelperApp.controller('TextHelperController', function ($scope) {
 
 	$scope.text='';
+	$scope.keyword1='';
+	$scope.keyword2='';
+	$scope.keyword3='';
+	$scope.keyword4='';
+	$scope.keyword5='';
+	$scope.keyword6='';
+	$scope.keyword7='';
+	$scope.keyword8='';
+	$scope.keyword9='';
+	$scope.keyword10='';
 	$scope.wordcount = function () { return $scope.text.split(/[^\s]+/).length - 1; };
+	$scope.keywordcount = function (keyword) {
+		var regex = new RegExp("/" + "test" + "/g");
+		return ($scope.text.match(/test/g) || []).length; 
+	};
 
 });
